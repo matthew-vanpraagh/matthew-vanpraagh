eb132cbe41f9fa1abb40731f30f6ee7120cf47e9
Matthew V <98849488+matthew-vanpraagh@users.noreply.github.com>
2023-04-04 17:55:37 -0400

    Add files via upload

---
+++ commandscale.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import manifold
+import pandas as pd
+import sklearn as sk
+from sklearn.decomposition import PCA
+
+read_data = pd.read_csv("jaccard_sample_2.csv")
+df = pd.DataFrame(read_data)
+labels = pd.read_csv("condensed.csv")
+
+df.to_numpy()
+
+print(df.shape)
+"""
+pca_songs = PCA(n_components=2)
+principalComponents_songs = pca_songs.fit_transform(df)
+df = principalComponents_songs
+
+print(df.shape)
+"""
+#df = pd.DataFrame(df, columns = ['PCA 1', 'PCA 2'])
+
+print(df.head)
+
+songs = labels["Track"]
+d = np.array([df])
+
+print(d)
+
+print(d.shape)
+
+d = d.reshape(d.shape[1], d.shape[2])
+
+print(d)
+
+print(d.shape)
+
+d = np.delete(d, 0, 1)
+
+print(d)
+
+print(d.shape)
+
+d = sk.utils.validation.check_symmetric(d, tol=1e-10, raise_warning=True, raise_exception=False)
+
+mds = manifold.MDS(1001, dissimilarity='precomputed')
+coords = mds.fit_transform(d)
+x, y = coords[:, 0], coords[:, 1]
+
+fig, ax = plt.subplots()
+ax.scatter(x, y)
+for (song, _x, _y) in zip(songs, x, y):
+    ax.annotate(song, (_x, _y))
+plt.show()
