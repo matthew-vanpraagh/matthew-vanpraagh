091ba74f0b96418a255a18b455f9c61569a02edd
Matthew V <98849488+matthew-vanpraagh@users.noreply.github.com>
2023-04-17 19:59:41 -0400

    Update jaccard.py

--- jaccard.py
+++ jaccard.py
@@ -8,7 +8,6 @@ import pandas as pd
 import plotly.graph_objs as go
 import warnings
 warnings.filterwarnings("ignore")
-import kmeans1d
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 from scipy.stats import gaussian_kde
@@ -18,14 +17,32 @@ df = pd.read_csv("ones_removed.csv")
 
 #df_condensed = df.iloc[0:25000]
 
+subset = df[['Artist', 'Track', 'Album']]
+tuples = [tuple(x) for x in subset.to_numpy()]
+df['new_tuples'] = list(zip(df['Artist'], df['Track'], df['Album']))
+
+print(df['new_tuples'].head())
+
+keep = df['new_tuples']
+
 from sklearn.metrics.pairwise import pairwise_distances
 jaccard = 1 - pairwise_distances(df.to_numpy(), metric='jaccard')
 
+df = pd.DataFrame(jaccard)
+
+print(df.columns)
+
+df['Records'] = keep
+
+df.set_index('Records')
+
+print(df.shape)
+
+df.to_csv('jaccard_sample_2.csv')
+print('Csv printed')
+
 print(jaccard)
 
-pd.DataFrame(jaccard).to_csv('jaccard_sample_2.csv')
-#jaccard.to_csv("jaccard_sample.csv")
-print("Csv printed")
 
 """
 song1 = input("Name of first song: ")
